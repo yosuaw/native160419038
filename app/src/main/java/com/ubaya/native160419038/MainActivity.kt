2cf15a905e6f05b5527c1faec9abbb07049a5a1f
Yosua Wijaya <yosuawijaya089@gmail.com>
2022-02-10 11:45:32 +0700

    variable login added

--- app/src/main/java/com/ubaya/native160419038/MainActivity.kt
+++ app/src/main/java/com/ubaya/native160419038/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    //Perlu bikin fitru login
+    var login:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
