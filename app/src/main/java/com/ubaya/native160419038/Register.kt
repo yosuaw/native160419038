c776a1c08c875e96771006e7ce7349462da1e5aa
Joshua Albertus S <joshuasamarianto@gmail.com>
2022-02-12 13:05:07 +0700

    Revert "registration class added"
    This reverts commit 8ecd1133

--- app/src/main/java/com/ubaya/native160419038/Register.kt
+++
@@ -1,7 +0,0 @@
-package com.ubaya.native160419038
-
-class Register(var username:String, var password:String, var firstName:String, var lastName:String) {
-    override fun toString(): String {
-        return "$firstName $lastName"
-    }
-}
