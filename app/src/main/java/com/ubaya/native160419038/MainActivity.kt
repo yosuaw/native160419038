9cc5ef37faff656e6e9decf2a07d577271772929
Joshua Albertus S <joshuasamarianto@gmail.com>
2022-02-12 12:02:00 +0700

    Benerin comment typo dan ganti value sebuah variabel

--- app/src/main/java/com/ubaya/native160419038/MainActivity.kt
+++ app/src/main/java/com/ubaya/native160419038/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    //Perlu bikin fitru login
+    //Perlu bikin fitur login
     var login:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login = true
     }
 }
